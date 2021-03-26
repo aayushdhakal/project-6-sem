38d493d7ab39c13c74a889abff67704c2d31294e
Aayush Dhaka <aayushdhakal96@gmail.com>
2021-03-26 17:46:40 +0545

    added search option and worked on signup page

--- scripts/src.js
+++ scripts/src.js
@@ -25,19 +25,40 @@ function showSlides(n) {
    for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
    }
-   slides[slideIndex-1].style.display = "block";
-   dots[slideIndex-1].className += " active"; 
+   slides[slideIndex - 1].style.display = "block";
+   dots[slideIndex - 1].className += " active";
 }
 
 setInterval(() => {
-   let temp = slideIndex || 1;
-   if (slideIndex > slides.length){
-      slideIndex=1;
-   }else{
+   if (slideIndex > slides.length) {
+      slideIndex = 1;
+   } else {
       slideIndex;
    }
    currentSlide(slideIndex);
    slideIndex++;
 }, 3000)
 
-//Assgining the blog nameing
+// make the input field visible
+const displayInputFieldBtn = document.querySelector('.search__form__btn');
+const inputFieldToBeShown = document.querySelector('.search__form__field');
+
+let visibleInputField = false;
+
+displayInputFieldBtn.addEventListener('click', changeVisiblility);
+
+inputFieldToBeShown.addEventListener('blur',()=>{
+   inputFieldToBeShown.classList.remove('search__form__field--display');
+   visibleInputField = false;
+})
+
+function changeVisiblility(e) {
+   visibleInputField = !visibleInputField;
+   if (visibleInputField) {
+      inputFieldToBeShown.classList.add('search__form__field--display');
+      inputFieldToBeShown.focus();
+   } else {
+      inputFieldToBeShown.classList.remove('search__form__field--display');
+   }
+}
+
